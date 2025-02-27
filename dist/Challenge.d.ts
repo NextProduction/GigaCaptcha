3f8670651ed7d6eaa3b620f65279551586cc40b1
Mahdi Hazrati <mahdihazratimailbox@gmail.com>
2025-02-27 10:58:33 +0330

    first version build

---
+++ dist/Challenge.d.ts
@@ -0,0 +1,4 @@
+declare const Challenge: ({ onSolve }: {
+    onSolve: () => void;
+}) => import("react/jsx-runtime").JSX.Element;
+export default Challenge;
---
+++ dist/GigaCaptcha.d.ts
@@ -0,0 +1,4 @@
+declare const GigaCaptcha: ({ onSuccess }: {
+    onSuccess: () => void;
+}) => import("react/jsx-runtime").JSX.Element;
+export default GigaCaptcha;
