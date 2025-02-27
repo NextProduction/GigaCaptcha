fdf62d03a0c02337f784e7036d8818f7a40ab48a
Mahdi Hazrati <mahdihazratimailbox@gmail.com>
2025-02-27 11:47:12 +0330

    Update package.json and enhance component imports for React

--- package/components/Challenge.tsx
+++ package/components/Challenge.tsx
@@ -1,3 +1,5 @@
+import React from 'react';
+
 const Challenge = ({ onSolve }: { onSolve: () => void }) => {
   return (
     <div>
--- package/components/GigaCaptcha.tsx
+++ package/components/GigaCaptcha.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import Challenge from "./Challenge";
 
 const GigaCaptcha = ({ onSuccess }: { onSuccess: () => void }) => {
