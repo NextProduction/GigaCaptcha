c2fbe904b4718ec64cdc7f86c298faa4b8252572
Mahdi Hazrati <mahdihazratimailbox@gmail.com>
2025-04-12 00:50:27 +0330

    clean project to new version

--- dist/Challenge.d.ts
+++
@@ -1,4 +0,0 @@
-declare const Challenge: ({ onSolve }: {
-    onSolve: () => void;
-}) => import("react/jsx-runtime").JSX.Element;
-export default Challenge;
--- dist/GigaCaptcha.d.ts
+++
@@ -1,4 +0,0 @@
-declare const GigaCaptcha: ({ onSuccess }: {
-    onSuccess: () => void;
-}) => import("react/jsx-runtime").JSX.Element;
-export default GigaCaptcha;
